893a9ff060d41837021ff58e6d90fcd8c2fe41f2
menthus123 <MarvelJohnson@Hotmail.com>
2022-02-17 14:09:24 -0800

    Added boilerplate to Home.vue

---
+++ client/src/pages/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <Nav />
+    <main>
+
+    </main>
+    <footer>
+      
+    </footer>
+  </div>
+</template>
+
+<script>
+import Nav from '../components/Nav'
+import Profile from '../components/Profile'
+import Technologies from '../components/Techonologies'
+import Projects from '../components/Projects'
+import Resume from '../components/Resume'
+
+export default {
+  name: 'Home',
+  components: {
+    Nav,
+    Profile,
+    Technologies,
+    Projects,
+    Resume
+  }
+}
+</script>
+
+<style>
+
+</style>
