f90d3f2ff5260542d4b42dd8927454d08ca357bd
menthus123 <MarvelJohnson@Hotmail.com>
2022-02-17 15:50:29 -0800

    Added hambuger menu

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,6 +1,19 @@
 <template>
   <nav>
-    <i class="fas fa-bars"></i>
+    <Transition>
+      <button class="hamburger main fas fa-bars" @click="toggleMobileSlideout" v-if="!navMobileSlideout"></button>
+    </Transition>
+    <Transition>
+      <div class="nav-mobile surface1" @click="toggleMobileSlideout" v-if="navMobileSlideout">
+        <button class="hamburger fas fa-bars"></button>
+        <ul class="nav-options">
+          <li><a href="#">About Me</a></li>
+          <li><a href="#">Technologies</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Resume</a></li>
+        </ul>
+      </div>
+    </Transition>
     <div class="nav-desktop hidden">
       <div class="nav-top">
         <p>MJ</p>
@@ -17,18 +30,92 @@
 
 <script>
 export default {
-  name: 'Nav'
+  name: 'Nav',
+  data: () => ({
+    navMobileSlideout: false
+  }),
+  methods: {
+    toggleMobileSlideout(){
+      this.navMobileSlideout = !this.navMobileSlideout;
+    }
+  }
 }
 </script>
 
 <style>
+  nav {
+    position: sticky;
+  }
+
   .nav-desktop {
     display: flex;
     justify-content: space-between;
     border-bottom: 1px solid rgba(0, 0, 0, .2);
   }
 
+  .nav-mobile {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 95vw;
+    height: 100vh;
+    padding: 0.5rem;
+    box-sizing: border-box;
+    transform: translateX(0);
+  }
+
+ .nav-mobile ul {
+   list-style: none;
+   font-size: 1.3rem;
+   margin-top: 2rem;
+   padding: 0 1.1rem;
+ }
+
+  .nav-mobile a {
+    text-decoration: none;
+    color: var(--text1);
+  }
+
+  .nav-mobile ul li {
+    padding-bottom: 0.5em;
+    border-bottom: 1px solid var(--text1);
+  }
+
+  .nav-mobile ul li + li {
+    margin-top: 0.5em;
+  }
+
+  .nav-mobile.v-enter-active {
+    transition: transform 0.3s 0.2s ease-in-out;
+  }
+
+  .nav-mobile.v-leave-active {
+    transition: transform 0.3s ease-in-out;
+  }
+
+  .nav-mobile.v-enter-from,
+  .nav-mobile.v-leave-to {
+    transform: translateX(-100%);
+  }
+
   .nav-top {
     box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.3);
+  }
+
+  .hamburger {
+    background-color: unset;
+    border: none;
+    font-size: 2rem;
+    color: var(--text1);
+  }
+
+  .hamburger.main.v-enter-active,
+  .hamburger.main.v-leave-active {
+    transition: opacity 0.1s ease-in-out;
+  }
+
+  .hamburger.main.v-enter-from,
+  .hamburger.main.v-leave-to {
+    opacity: 0;
   }
 </style>
