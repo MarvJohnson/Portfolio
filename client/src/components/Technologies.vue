05befba2d95f34f55014fd9225f692b94e63e9c8
menthus123 <MarvelJohnson@Hotmail.com>
2022-02-18 04:55:05 -0800

    Added base content and styling for Technologies.vue as well as Technology.vue

--- client/src/components/Technologies.vue
+++ client/src/components/Technologies.vue
@@ -1,15 +1,55 @@
 <template>
   <div class="technologies">
-
+    <h3>Technologies I know</h3>
+    <div>
+      <Technology v-for="(technology, index) in technologies" :key="index" :icon="technology.icon" :label="technology.label" />
+    </div>
   </div>
 </template>
 
 <script>
+import Technology from './Technology'
+
 export default {
-  name: 'Technologies'
+  name: 'Technologies',
+  components: {
+    Technology
+  },
+  data: () => ({
+    technologies: [
+      { icon: "['fas', fa-brands fa-html5]", label: 'HTML5' },
+      { icon: "['fas', fa-brands fa-css3-alt]", label: 'CSS3' },
+      { icon: "['fas', fa-brands fa-js]", label: 'Javascript' },
+      { icon: "['fas', fa-brands fa-css3-alt]", label: 'CSS3' },
+      { icon: "['fas', fa-brands fa-js]", label: 'Javascript' },
+      { icon: "['fas', fa-brands fa-css3-alt]", label: 'CSS3' },
+      { icon: "['fas', fa-brands fa-js]", label: 'Javascript' },
+      { icon: "['fas', fa-brands fa-css3-alt]", label: 'CSS3' },
+      { icon: "['fas', fa-brands fa-js]", label: 'Javascript' },
+      { icon: "['fas', fa-brands fa-css3-alt]", label: 'CSS3' },
+      { icon: "['fas', fa-brands fa-js]", label: 'Javascript' },
+      { icon: "['fas', fa-brands fa-js]", label: 'Javascript' },
+    ]
+  })
 }
 </script>
 
 <style>
+  .technologies {
+    font-size: 1rem;
+  }
 
+  .technologies > h3 {
+    border-bottom: 1px solid var(--surface1);
+    padding-bottom: 0.2em;
+    margin: 1em;
+  }
+
+  .technologies > div {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    text-align: center;
+    gap: 1em;
+    margin: 0 1rem;
+  }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faHtml5,
+  faCss3Alt,
+  faNodeJs,
+  faGithub,
+  faLinkedin
+} from '@fortawesome/free-brands-svg-icons';
+// import { far } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-createApp(App).mount('#app')
+library.add(faHtml5, faCss3Alt, faNodeJs, faGithub, faLinkedin);
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
