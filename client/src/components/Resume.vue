8dbe375e0c56aca4f62a2dd217861ddbed2ef8fe
menthus123 <MarvelJohnson@Hotmail.com>
2022-02-18 07:38:37 -0800

    Added ability to download image of resume

--- client/src/components/Resume.vue
+++ client/src/components/Resume.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="resume">
     <img src="@/assets/resume.png" alt="">
-    <a href="@/assets" download="resume"><button>Download</button></a>
+    <button @click="downloadResume">Download</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import resume from '../assets/resume.png'
+
 export default {
-  name: 'Resume'
+  name: 'Resume',
+  methods: {
+    async downloadResume(){
+      const result = await axios.get(resume, { responseType: 'blob' });
+      const blob = new Blob([result.data], { type: 'image/png' });
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = 'resume';
+      link.click();
+      URL.revokeObjectURL(link.href);
+    }
+  }
 }
 </script>
 
---
+++ client/src/components/Technology.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="technology">
+    <font-awesome-icon :icon="icon"></font-awesome-icon>
+    <p>{{ label }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Technology',
+  props: {
+    icon: String,
+    label: String
+  }
+}
+</script>
+
+<style>
+  .technology {
+    border: 1px solid rgba(0, 0, 0, 0.3);
+  }
+</style>
