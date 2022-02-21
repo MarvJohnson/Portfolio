51db79672fc6b87d8fecbd9f9b345da39071abf4
menthus123 <MarvelJohnson@Hotmail.com>
2022-02-21 15:20:07 -0800

    Updated service config for backend

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const BASE_URL =
-  process.env.VUE_APP_MODE === 'prod'
-    ? `https://mj-portfolio-back-end.herokuapp.com/`
-    : 'http://localhost:8000/';
+  process.env.VUE_APP_MODE === 'dev'
+    ? 'http://localhost:8000/'
+    : `https://mj-portfolio-back-end.herokuapp.com/`;
 
 const Client = axios.create({
   baseURL: BASE_URL
